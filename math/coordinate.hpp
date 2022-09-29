44dc8ea77f7b6b1d4d0501434046a180c4551ecb
ZenithGD <darimarcos456@hotmail.com>
2022-09-30 00:09:47 +0200

    doc coordinate

--- math/coordinate.hpp
+++ math/coordinate.hpp
@@ -83,16 +83,68 @@ public:
  */
 ostream& operator<<(ostream& os, Coordinate coor);
 
+/**
+ * @brief Translates coord by a translation vector v.
+ * 
+ * @param coord The coordinate object
+ * @param v The translation vector
+ * @return Coordinate 
+ */
 Coordinate translation(const Coordinate coord, const Vector3 v);
 
+/**
+ * @brief Rotate the coordinate system by theta radians on the X axis.
+ * 
+ * @param coord The coordinate object
+ * @param theta The rotation angle, in radians 
+ * @return Coordinate 
+ */
 Coordinate rotationX(const Coordinate coord, float theta);
 
+/**
+ * @brief Rotate the coordinate system by theta radians on the Y axis.
+ * 
+ * @param coord The coordinate object
+ * @param theta The rotation angle, in radians 
+ * @return Coordinate 
+ */
 Coordinate rotationY(const Coordinate coord, float theta);
 
+/**
+ * @brief Rotate the coordinate system by theta radians on the Z axis.
+ * 
+ * @param coord The coordinate object
+ * @param theta The rotation angle, in radians 
+ * @return Coordinate 
+ */
 Coordinate rotationZ(const Coordinate coord, float theta);
 
+/**
+ * @brief Scale the coordinate system by a scale vector
+ * This will scale by v.x on the X axis, v.y on the Y axis, and v.z on the Z axis
+ * @param coord The coordinate object
+ * @param v The scale vector
+ * @return Coordinate 
+ */
 Coordinate scale(const Coordinate coord, const Vector3 v);
 
+/**
+ * @brief Change the coordinate basis.
+ * This will apply a change of basis from the new one to the current coordinate system
+ * @param coord The coordinate object
+ * @param u The new basis's first axis in the old basis's coordinates
+ * @param v The new basis's second axis in the old basis's coordinates
+ * @param w The new basis's third axis in the old basis's coordinates
+ * @param o The new basis's origin in the old basis's coordinates
+ * @return Coordinate 
+ */
 Coordinate changeBasis(const Coordinate coord, const Vector3 u, const Vector3 v, const Vector3 w, const Vector3 o);
 
+/**
+ * @brief Return the inverse transform of the coordinate object.
+ * Applying the inverse transformation to the coordinate object should return
+ * an identity coordinate object.
+ * @param coor The coordinate object
+ * @return Coordinate 
+ */
 Coordinate inverseTransformation(Coordinate coor);
